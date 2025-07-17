f6c410b658a80de2696edd8933a11a2100650f61
udragan <ujfa.subs+github@gmail.com>
2025-07-17 22:44:07 +0200

    added empty auth service (#1)

---
+++ solution/ApiGateway/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace ApiGateway.Middleware;
+
+public class RequestLoggingMiddleware
+{
+	#region Members
+	private readonly RequestDelegate _next;
+	private readonly ILogger _logger;
+	#endregion
+
+	#region Constructors
+	public RequestLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+	{
+		_next = next;
+		_logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
+	}
+	#endregion
+
+	#region Public methods
+	public async Task Invoke(HttpContext context)
+	{
+		_logger.LogInformation($"Incoming request: {context.Request.Method} {context.Request.Path}");
+		await _next(context);
+	}
+	#endregion
+}
---
+++ solution/ApiGateway/Services/RoutingService.cs
@@ -0,0 +1,43 @@
+﻿namespace ApiGateway.Services;
+
+public class RoutingService
+{
+	#region Members
+	private readonly HttpClient _httpClient;
+	private readonly IConfiguration _config;
+	#endregion
+
+	#region Constructors
+	public RoutingService(HttpClient httpClient, IConfiguration config)
+	{
+
+		_httpClient = httpClient;
+		_config = config;
+	}
+	#endregion
+
+	#region Public methods
+	public async Task<HttpResponseMessage> ForwardRequestAsync(HttpRequest request)
+	{
+		var routeKey = request.Path.Value?.Split('/')[1];
+		var baseUrl = _config[$"Routes:{routeKey}"];
+
+		if (string.IsNullOrEmpty(baseUrl))
+			return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest) { ReasonPhrase = "Invalid route" };
+
+		var forwardUrl = baseUrl + request.Path.Value?.Substring(routeKey.Length + 1) + request.QueryString;
+
+		var forwardRequest = new HttpRequestMessage(new HttpMethod(request.Method), forwardUrl)
+		{
+			Content = new StreamContent(request.Body)
+		};
+
+		foreach (var header in request.Headers)
+		{
+			forwardRequest.Headers.TryAddWithoutValidation(header.Key, header.Value.ToArray());
+		}
+
+		return await _httpClient.SendAsync(forwardRequest);
+	}
+	#endregion
+}
