9be8f69b1ce10e47773f6f1946cf1d04b3ae2ac1
udragan <ujfa.subs+github@gmail.com>
2025-07-27 22:17:29 +0200

    Book service docker (#15)
    * book-service docker
    
    added book-service to docker deploy

--- solution/services/book-service/app/db/database.py
+++ solution/services/book-service/app/db/database.py
@@ -1,8 +1,9 @@
 # database.py
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://dev:dev@localhost:6003/book-service"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
