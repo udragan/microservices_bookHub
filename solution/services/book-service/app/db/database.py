91528e76fa8e231bbbcf1e38ca61bb30d47ce35d
udragan <ujfa.subs+github@gmail.com>
2025-07-31 19:06:23 +0200

    book-service added authorization (#17)
    * book-service - added authorization
    
    * book-service - logging and cleanup

--- solution/services/book-service/app/db/database.py
+++ solution/services/book-service/app/db/database.py
@@ -1,9 +1,9 @@
-# database.py
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(f"INFO:   DATABASE_URL={DATABASE_URL}")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
--- solution/services/book-service/app/main.py
+++ solution/services/book-service/app/main.py
@@ -1,13 +1,14 @@
-# main.py
 import app.env
 
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
+from app.auth.authorization import JwtUser, get_current_user, is_admin
 from app.db.database import AsyncSessionLocal, engine, Base
 from app.models.book import Book
-from app.schemas.book import BookCreate
+from app.schemas.book import BookBody
+
 
 app = FastAPI()
 
@@ -15,22 +16,29 @@ app = FastAPI()
 @app.on_event("startup")
 async def on_startup():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
+      	await conn.run_sync(Base.metadata.create_all)
+          
 # Dependency to get DB session
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
 
-@app.post("/books/")
-async def create_book(book: BookCreate, db: AsyncSession = Depends(get_db)):
+# endpoints ###########################
+
+@app.post("/")
+async def create_book(book: BookBody,
+        jwt: JwtUser = Depends(get_current_user),  
+        db: AsyncSession = Depends(get_db)):
+    if not is_admin(jwt):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to perform this action.")
+    
     new_book = Book(name=book.name, author=book.author)
     db.add(new_book)
     await db.commit()
     await db.refresh(new_book)
     return new_book
 
-@app.get("/books/")
+@app.get("/")
 async def list_books(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Book))
     return result.scalars().all()
