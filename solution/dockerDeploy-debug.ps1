c9253249c6c35d1e9f659e5ade90a1871cebdab9
udragan <ujfa.subs+github@gmail.com>
2025-08-05 17:16:32 +0200

    Simplified overrides for docker-compose-debug (#24)

--- solution/dockerDeploy-debug.ps1
+++ solution/dockerDeploy-debug.ps1
@@ -1,5 +1,5 @@
 Set-Location -Path $PSScriptRoot
 
-docker compose -p bookhub-debug -f docker-compose-debug.yml up --build -d
+docker compose -p bookhub-debug -f docker-compose.yml -f docker-compose-debug.yml up --build -d
 
 Read-Host -Prompt "Press Enter to exit"
