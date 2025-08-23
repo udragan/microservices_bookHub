c6fe699ccd865e7195c6e3a53201e730b4271cfd
udragan <ujfa.subs+github@gmail.com>
2025-08-23 13:07:54 +0200

    review-service - added book model

---
+++ solution/services/review-service/app/db/models/book.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Book represents a document in the "books" collection
+type Book struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	BookId    int                `bson:"bookId"`
+	Timestamp string             `bson:"timestamp"`
+}
