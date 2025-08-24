64fe6acc9bb9f115d19220e300d3011f442a74c9
udragan <ujfa.subs+github@gmail.com>
2025-08-24 10:03:30 +0200

    review-service - books bookId unique migration
    Added migration to make bookId in books collection unique

---
+++ solution/services/review-service/app/db/migrations/000004_books-unique-book-id.up.mjs
@@ -0,0 +1,12 @@
+[
+    {
+        "createIndexes": "books",
+        "indexes": [
+            {
+            "key": { "bookId": 1 },
+            "name": "idx_bookId_unique",
+            "unique": true
+            }
+        ]
+    }
+]
