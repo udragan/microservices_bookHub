9112b741980962f6877d746817550a1434410d21
udragan <ujfa.subs+github@gmail.com>
2025-08-25 20:33:42 +0200

    auth-service - new endpoint for generating service to service communication token (#44)

---
+++ solution/services/auth-service/Models/FromRequests/ServiceTokenRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuthService.Models.FromRequests;
+
+public record ServiceTokenRequest
+(
+	[property: JsonPropertyName("serviceName")] string ServiceName,
+	[property: JsonPropertyName("targetServiceName")] string TargetServiceName,
+	[property: JsonPropertyName("sharedSecret")] string SharedSecret
+);
