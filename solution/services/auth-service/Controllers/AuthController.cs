cfe93e14f865c95abdb730ea2e9770f493c6db73
udragan <ujfa.subs+github@gmail.com>
2025-08-25 20:32:39 +0200

    auth-service - new endpoint for generating service to service communication token

--- solution/services/auth-service/Controllers/AuthController.cs
+++ solution/services/auth-service/Controllers/AuthController.cs
@@ -6,10 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 namespace AuthService.Controllers;
 
 [ApiController]
-[Route("login")]
 public class AuthController(TokenService _tokenService, IUserService _userService) : ControllerBase
 {
-	[HttpPost]
+	[HttpPost("login")]
 	public async Task<IActionResult> Login([FromBody] LoginRequest request)
 	{
 		if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password))
@@ -20,7 +19,25 @@ public class AuthController(TokenService _tokenService, IUserService _userServic
 		(bool success, string? message, Models.User? user) = await _userService.Verify(request);
 
 		return success ?
-			Ok(new { access_token = _tokenService.GenerateToken(user!) }) :
+			Ok(new { access_token = _tokenService.GenerateUserToken(user!) }) :
 			Unauthorized(message);
+	}
+
+	[HttpPost("internal/token")]
+	public IActionResult ServiceToken([FromBody] ServiceTokenRequest request)
+	{
+		if (string.IsNullOrWhiteSpace(request.ServiceName) ||
+			string.IsNullOrWhiteSpace(request.TargetServiceName) ||
+			string.IsNullOrWhiteSpace(request.SharedSecret))
+		{
+			return Unauthorized("no data");
+		}
+
+		if (!string.Equals(request.SharedSecret, Environment.GetEnvironmentVariable("SHARED_SECRET")))
+		{
+			return Unauthorized("invalid credentials");
+		}
+
+		return Ok(new { access_token = _tokenService.GenerateServiceToken(request.ServiceName, request.TargetServiceName) });
 	}
 }
--- solution/services/auth-service/Services/Implementations/TokenService.cs
+++ solution/services/auth-service/Services/Implementations/TokenService.cs
@@ -6,7 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 namespace AuthService.Services.Implementations;
 public class TokenService(RsaSecurityKey _key, string _issuer)
 {
-	public string GenerateToken(User user)
+	public string GenerateUserToken(User user)
 	{
 		Claim[] claims =
 		[
@@ -20,9 +20,30 @@ public class TokenService(RsaSecurityKey _key, string _issuer)
 
 		JwtSecurityToken token = new(
 			issuer: _issuer,
-			audience: "bookhub-api-gateway",
+			audience: Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
 			claims: claims,
 			expires: DateTime.UtcNow.AddHours(1),
+			signingCredentials: credentials
+		);
+
+		return new JwtSecurityTokenHandler().WriteToken(token);
+	}
+
+	public string GenerateServiceToken(string serviceName, string targetServiceName)
+	{
+		Claim[] claims =
+		[
+			new Claim(JwtRegisteredClaimNames.Sub, serviceName),
+			new Claim(JwtRegisteredClaimNames.Azp, targetServiceName)
+		];
+
+		SigningCredentials credentials = new SigningCredentials(_key, SecurityAlgorithms.RsaSha256);
+
+		JwtSecurityToken token = new(
+			issuer: _issuer,
+			audience: Environment.GetEnvironmentVariable("JWT_AUDIENCE"),
+			claims: claims,
+			expires: DateTime.UtcNow.AddMinutes(5),
 			signingCredentials: credentials
 		);
 
