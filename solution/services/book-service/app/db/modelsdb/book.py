8cbffbdd2323cb26174b115e204ccca642c6ae2b
udragan <ujfa.subs+github@gmail.com>
2025-08-31 12:24:54 +0200

    book-service - add soft delete flag to Book entity (#49)

--- solution/services/book-service/app/db/modelsdb/book.py
+++ solution/services/book-service/app/db/modelsdb/book.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean, text
 from app.db.database import Base
 
 class Book(Base):
@@ -7,3 +7,4 @@ class Book(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
     author = Column(String, nullable=False)
+    isDeleted = Column(Boolean, nullable=False, server_default=text('false'))
--- solution/services/book-service/app/models/dtos/book_dto.py
+++ solution/services/book-service/app/models/dtos/book_dto.py
@@ -4,6 +4,7 @@ class BookDto(BaseModel):
     id: int
     title: str
     author: str
+    isDeleted: bool
 
     model_config = {
         "from_attributes": True
