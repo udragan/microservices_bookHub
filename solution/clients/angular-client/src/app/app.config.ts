94b0ba92af832275fca792e63c238964973e631e
udragan <ujfa.subs+github@gmail.com>
2025-09-18 16:16:47 +0200

    angular-client - added taigaUi kit

--- solution/clients/angular-client/src/app/app.config.ts
+++ solution/clients/angular-client/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
 
 import { routes } from './app.routes';
 
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
 		provideBrowserGlobalErrorListeners(),
 		provideZonelessChangeDetection(),
 		provideRouter(routes),
-		provideHttpClient()
+		provideHttpClient(),
+		provideEventPlugins(),
 	]
 };
