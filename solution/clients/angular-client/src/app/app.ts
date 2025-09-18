226b696512c835798da2f78cc8a8c71737a5f8ae
udragan <ujfa.subs+github@gmail.com>
2025-09-18 16:17:31 +0200

    angular-client - added taigaUi kit (#63)

--- solution/clients/angular-client/src/app/app.ts
+++ solution/clients/angular-client/src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TuiRoot } from '@taiga-ui/core'
 
 import { AuthInterceptor } from './features/auth/auth.interceptor';
 
 @Component({
 	selector: 'app-root',
 	imports: [
-		RouterOutlet],
+		RouterOutlet,
+		TuiRoot,
+	],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 	providers: [
