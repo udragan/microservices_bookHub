9444471456fa06bb3aaf57f5ba85a5f64b0a5513
udragan <ujfa.subs+github@gmail.com>
2025-09-30 17:21:02 +0200

    angular-client - added global header (#67)

--- solution/clients/angular-client/src/app/app.routes.ts
+++ solution/clients/angular-client/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { Login } from './features/auth/login';
 import { Home } from './features/home/home';
 import { AdminGuard } from './features/admin/admin.guard';
 import { AdminDashboard } from './features/admin/admin-dashboard';
+import { AdminUsers } from './features/admin/admin-users';
 
 export const routes: Routes = [    
 		{ path: '', component: Home, canActivate: [AuthGuard] },
 		{ path: 'login', component: Login },
-		{ path: 'admin-dashboard', component: AdminDashboard, canActivate: [AdminGuard] }
+		{ path: 'admin-dashboard', component: AdminDashboard, canActivate: [AdminGuard] },
+		{ path: 'admin-users', component: AdminUsers, canActivate: [AdminGuard] }
 ];
 
 export const AppRoutes = {
--- solution/clients/angular-client/src/app/app.ts
+++ solution/clients/angular-client/src/app/app.ts
@@ -1,26 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TuiRoot } from '@taiga-ui/core'
+import { TuiAppearance, TuiDataList, TuiDropdown, TuiRoot, TuiTextfield } from '@taiga-ui/core'
 
 import { AuthInterceptor } from './features/auth/auth.interceptor';
+import { TuiAvatar, TuiBadgeNotification, TuiTabs } from '@taiga-ui/kit';
+import { TuiNavigation } from '@taiga-ui/layout';
+import { AuthService } from './features/auth/auth.service';
 
 @Component({
 	selector: 'app-root',
 	imports: [
 		RouterOutlet,
-		TuiRoot
+		TuiRoot,
+		TuiBadgeNotification,
+		TuiAvatar,		
+		TuiAppearance,
+		TuiAvatar,
+		TuiBadgeNotification,
+		TuiDataList,
+		TuiDropdown,
+		TuiTabs,
+		TuiTextfield,
+		TuiNavigation,
 	],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 	providers: [
-		{
-				provide: HTTP_INTERCEPTORS,
-				useClass: AuthInterceptor,
-				multi: true
-		}
+	{
+		provide: HTTP_INTERCEPTORS,
+		useClass: AuthInterceptor,
+		multi: true
+	}
 	]
 })
 export class App {
 	protected readonly title = signal('Bookhub-AngularClient');
+
+	constructor(public auth: AuthService) {}
 }
