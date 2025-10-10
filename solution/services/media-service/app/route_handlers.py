1d7d91dc7eab1128a5f15058f1849b35526705c5
udragan <ujfa.subs+github@gmail.com>
2025-10-10 19:44:30 +0200

    media-service - added remove avatar endpoint (#70)

--- solution/services/media-service/app/route_handlers.py
+++ solution/services/media-service/app/route_handlers.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from fastapi import UploadFile, HTTPException, status
 from fastapi.responses import FileResponse
@@ -5,23 +6,40 @@ from fastapi.responses import FileResponse
 from app.auth.authorization import JwtUser, is_admin
 
 MEDIA_DIR = os.getenv("MEDIA_STORAGE_PATH")
-os.makedirs(MEDIA_DIR, exist_ok=True)
+AVATARS_PATH = os.path.join(MEDIA_DIR, "avatars")
+os.makedirs(AVATARS_PATH, exist_ok=True)
 
 async def upload_avatar(file: UploadFile,
             jwt: JwtUser) -> str | HTTPException:
-    saved_filename = f"avatar_{jwt.id}.png"
-    file_path = os.path.join(MEDIA_DIR, saved_filename)
+    if not await remove_avatar(jwt):
+        return
+    file_ext = os.path.splitext(file.filename)[1]
+    filename = f"{jwt.id}{file_ext}"
+    file_path = os.path.join(AVATARS_PATH, filename)
     try:
         with open(file_path, "wb") as buffer:
             content = await file.read()
             buffer.write(content)
     except:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, detail="Unable to save media file")
-    return {"avatar_path": saved_filename}
+    return {"avatar_path": filename}
 
 async def get_avatar(jwt: JwtUser) -> FileResponse | HTTPException:
-    avatar_file_name = f"avatar_{jwt.id}.png"
-    file_path = os.path.join(MEDIA_DIR, avatar_file_name)
-    if not os.path.exists(file_path):
+    filename = jwt.id
+    file_path = os.path.join(AVATARS_PATH, filename)
+    matching_files = glob.glob(f"{file_path}.*")
+    if not matching_files:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File not found")
-    return FileResponse(file_path)
+    return FileResponse(matching_files[0])
+
+async def remove_avatar(jwt: JwtUser) -> bool | HTTPException:
+    filename = jwt.id
+    file_path = os.path.join(AVATARS_PATH, filename)
+    matching_files = glob.glob(f"{file_path}.*")
+    if not matching_files:
+        return True
+    try:
+        os.remove(matching_files[0])
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Remove avatar for user {jwt.email} failed")
+    return True
