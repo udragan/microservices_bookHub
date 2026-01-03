ad3fa97ce1c1106e6ffc334483ff36e7aeba2323
udragan <ujfa.subs+github@gmail.com>
2026-01-03 19:30:23 +0100

    angular-client - added admin-main-layout page (#79)

--- solution/clients/angular-client/src/app/app.routes.ts
+++ solution/clients/angular-client/src/app/app.routes.ts
@@ -2,21 +2,28 @@ import { Routes } from '@angular/router';
 
 import { AuthGuard } from './features/auth/auth.guard';
 import { Login } from './features/auth/login';
-import { Home } from './features/home/home';
 import { AdminGuard } from './features/admin/admin.guard';
+import { AdminMainLayout } from './features/admin/admin-main-layout';
 import { AdminDashboard } from './features/admin/admin-dashboard';
+import { Home } from './features/home/home';
 import { AdminUsers } from './features/admin/admin-users';
 
 export const routes: Routes = [    
 		{ path: '', component: Home, canActivate: [AuthGuard] },
 		{ path: 'login', component: Login },
-		{ path: 'admin-dashboard', component: AdminDashboard, canActivate: [AdminGuard] },
-		{ path: 'admin-users', component: AdminUsers, canActivate: [AdminGuard] }
+		{
+			path: 'admin',
+			component: AdminMainLayout,
+			children: [
+				{ path: 'dashboard', component: AdminDashboard, canActivate: [AdminGuard] },
+				{ path: 'users', component: AdminUsers, canActivate: [AdminGuard] },
+			]
+		}
 ];
 
 export const AppRoutes = {
 	Home: '/',
 	Login: '/login',
-	AdminDashboard: '/admin-dashboard',
-	AdminUsers: '/admin-users'
+	AdminDashboard: '/admin/dashboard',
+	AdminUsers: '/admin/users'
 }
