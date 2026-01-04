fd307583fe024c7234a9a6eccea7771a774afe0f
udragan <ujfa.subs+github@gmail.com>
2026-01-04 18:28:28 +0100

    feat (angular-client): Add AdminAccount page and upload avatar (#83)

--- solution/clients/angular-client/src/app/core/services/users.service.ts
+++ solution/clients/angular-client/src/app/core/services/users.service.ts
@@ -1,21 +1,33 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
 import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  	providedIn: 'root'
 })
 export class UsersService {
-  private apiUrl = '';
-
-  constructor(private http: HttpClient) {
+	private apiUrl = '';
+	
+	private sanitizer = inject(DomSanitizer);
+	
+	avatarUrlSignal = signal<any>(null);
+	
+	constructor(private http: HttpClient) {
 		this.apiUrl = environment.apiBaseUrl + "/users";
+	}
+
+	// ------------------------------------------------------------------------
+
+	updateAvatar(newAvatarUrl: string) {
+		const url = this.sanitizer.bypassSecurityTrustUrl(newAvatarUrl);
+		this.avatarUrlSignal.set(url);
 	}
 	
 	getAllUsers(): Observable<User[]> {
 		return this.http.get<User[]>(`${this.apiUrl}`);
-  }
+	}
 }
