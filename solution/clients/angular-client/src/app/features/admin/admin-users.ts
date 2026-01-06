7705a633b940a7591ea6369d9d6cf151046350cc
udragan <ujfa.subs+github@gmail.com>
2026-01-06 20:07:23 +0100

    feat (angular-client): AdminUsers reset password with confirmation (#94)

--- solution/clients/angular-client/src/app/features/admin/admin-users.ts
+++ solution/clients/angular-client/src/app/features/admin/admin-users.ts
@@ -1,12 +1,15 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { Button } from 'primeng/button';
+import { ConfirmDialog } from 'primeng/confirmdialog';
 import { Dialog } from 'primeng/dialog';
 import { FloatLabel } from 'primeng/floatlabel';
 import { InputText } from 'primeng/inputtext';
 import { Select } from "primeng/select";
 import { TableModule } from 'primeng/table';
+import { Toast } from 'primeng/toast';
 
 import { AppRoutes } from '../../app.routes';
 import { User } from '../../core/models/user.model';
@@ -16,18 +19,26 @@ import { UsersService } from '../../core/services/users.service';
 @Component({
 	selector: 'app-admin-users',
 	imports: [
-    Button,
-    Dialog,
-    FloatLabel,
-    FormsModule,
-    InputText,
-    TableModule,
-    Select
-],
+		Button,
+		ConfirmDialog,
+		Dialog,
+		FloatLabel,
+		FormsModule,
+		InputText,
+		TableModule,
+		Toast,
+		Select
+	],
+	providers: [
+		ConfirmationService,
+		MessageService
+	],
 	templateUrl:'./admin-users.html',
 	styleUrl: './admin-users.scss',
 })
 export class AdminUsers implements OnInit {
+	private confirmationService = inject(ConfirmationService)
+	private messageService = inject(MessageService);
 	private userService = inject(UsersService);
 	
 	protected routes = AppRoutes;
@@ -76,4 +87,33 @@ export class AdminUsers implements OnInit {
 			});	
 		}
 	}
+
+	confirmPasswordReset(event: Event, item: User) {
+		this.confirmationService.confirm({
+			target: event.target as EventTarget,
+			message: `Reset password for user: ${item.email}?`,
+			header: 'Confirmation',
+			icon: 'pi pi-question-circle',
+			acceptIcon: "none",
+			rejectIcon: "none",
+			rejectButtonStyleClass: "p-button-text",
+			accept: () => {
+				this.userService.passwordReset(item.id).subscribe({
+					next: _ => {
+						this.messageService.add({ 
+							severity: 'success', 
+							summary: 'Confirmed', 
+							detail: `${item.email} reset password successfully.` 
+						});
+					},
+					error: e => {
+						console.log(e);
+					}
+				})
+			},
+			reject: () => {
+				console.log('Rejected!');
+			}
+		});
+  	}
 }
